a388987bd515f12a6a643501dbeb3f2b996fd4a0
Matt Bechtel <matt.bechtel@kingland.com>
2024-03-06 10:40:31 -0600

    React app is now working, added a test component

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TestComponent from "./components/test/TestComponent";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, Welcome to React and TypeScript</h1>
+      <div>blahblah</div>
+      <TestComponent />
+    </div>
+  );
+};
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 module.exports = {
-    entry: './src/index.tsx',
+    entry: './src/App.tsx',
     module: {
         rules: [
         {
